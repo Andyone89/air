f4f7745d670b947dfdf085686e32da2cbd1f87c3
Andyone89 <11and.one89@gmail.com>
2025-03-02 13:40:09 +0200

    media

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 $(function () {
-$('.header__burger').click(function(event) {
+    $('.header__burger').click(function (event) {
         $('.header__burger, .menu, .header').toggleClass('active');
+    });
+});
+
+$(function () {
+    $('.charters__slider').slick({
+        Infinie: true,
+        dots: true,
+        slidesToShow: 4,
+        slidesToScroll: 4
     });
 });
